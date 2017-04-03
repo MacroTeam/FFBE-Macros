f58a9db9f8bb3acfed576dba1025c9964575417b
kastyr <kastyr1984@gmail.com>
2017-04-03 18:56:18 -0400

    Added in samples of Mitch3llO01's macros for demonstrative purposes.

---
+++ Mitch3llO01/convert.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 ~/Documents/code/convertMacros/convertMacros.py -i Nox/phantom_forest_1150_delay_\(mitch3llo01_4-3-2017\).txt -o Memu/phantom_forest_1150_delay_\(mitch3llo01_4-3-2017\).txt --outtype memu
+python3 ~/Documents/code/convertMacros/convertMacros.py -i Nox/tm_-ultimate-_patch_2.0_lapis_refill_\(mitch3llo01_3-31-2017\).txt -o Memu/tm_-ultimate-_patch_2.0_lapis_refill_\(mitch3llo01_3-31-2017\).txt --outtype memu
+python3 ~/Documents/code/convertMacros/convertMacros.py -i Nox/tm_-ultimate-_patch_2.0_no_lapis_\(mitch3llo01_3-31-2017\).txt -o Memu/tm_-ultimate-_patch_2.0_no_lapis_\(mitch3llo01_3-31-2017\).txt --outtype memu
+
+python3 ~/Documents/code/convertMacros/convertMacros.py -i Nox/phantom_forest_1150_delay_\(mitch3llo01_4-3-2017\).txt -o Hiro/phantom_forest_1150_delay_\(mitch3llo01_4-3-2017\).txt --outtype hiro
+python3 ~/Documents/code/convertMacros/convertMacros.py -i Nox/tm_-ultimate-_patch_2.0_lapis_refill_\(mitch3llo01_3-31-2017\).txt -o Hiro/tm_-ultimate-_patch_2.0_lapis_refill_\(mitch3llo01_3-31-2017\).txt --outtype hiro
+python3 ~/Documents/code/convertMacros/convertMacros.py -i Nox/tm_-ultimate-_patch_2.0_no_lapis_\(mitch3llo01_3-31-2017\).txt -o Hiro/tm_-ultimate-_patch_2.0_no_lapis_\(mitch3llo01_3-31-2017\).txt --outtype hiro
