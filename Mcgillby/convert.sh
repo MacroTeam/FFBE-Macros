71cacabc34deaff822ec220e76685d2e1eefd120
kastyr <kastyr1984@gmail.com>
2017-04-05 16:20:38 -0400

    Fixed an issue with convert command lines

--- Mcgillby/convert.sh
+++ Mcgillby/convert.sh
@@ -11,6 +11,6 @@ python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/no_refills_+_ti
 python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/tm_refills_720.txt' -o 'Hiro/tm_refills_720.txt' --in-x 720 --in-y 1280 --flip-y --outtype hiro
 
 python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/lb_farming_(es-exit)_no_refills_720.txt' -o 'MEmu/y-flipped/lb_farming_(es-exit)_no_refills_720.txt' --in-x 720 --in-y 1280 --flip-y --outtype memu
-python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/no_refills_720.txt' -o 'MEmu/y-flipped/no_refills_720-flipped.txt' --in-x 720 --in-y 1280 --flip-y --outtype hiro
-python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/no_refills_+_timer_720.txt' -o 'MEmu/y-flipped/no_refills_+_timer_720-flipped.txt' --in-x 720 --in-y 1280 --flip-y --outtype hiro
-python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/tm_refills_720.txt' -o 'MEmu/y-flipped/tm_refills_720-flipped.txt' --in-x 720 --in-y 1280 --flip-y --outtype hiro
+python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/no_refills_720.txt' -o 'MEmu/y-flipped/no_refills_720-flipped.txt' --in-x 720 --in-y 1280 --flip-y --outtype memu
+python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/no_refills_+_timer_720.txt' -o 'MEmu/y-flipped/no_refills_+_timer_720-flipped.txt' --in-x 720 --in-y 1280 --flip-y --outtype memu
+python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/tm_refills_720.txt' -o 'MEmu/y-flipped/tm_refills_720-flipped.txt' --in-x 720 --in-y 1280 --flip-y --outtype memu
