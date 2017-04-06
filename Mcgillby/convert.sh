0b98420870a7c107f4ac35227620e706ea33a63a
kastyr <kastyr1984@gmail.com>
2017-04-05 21:35:57 -0400

    Initial commit of Mcgillby small resolution niche macros for testing purposes

--- Mcgillby/convert.sh
+++ Mcgillby/convert.sh
@@ -5,7 +5,16 @@ python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/no_refills_720.
 python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/no_refills_+_timer_720.txt' -o 'Nox/no_refills_+_timer_720.txt' --in-x 720 --in-y 1280 --outtype nox
 python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/tm_refills_720.txt' -o 'Nox/tm_refills_720.txt' --in-x 720 --in-y 1280 --flip-y --outtype nox
 
+python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/no_refills_[480]_[memu].txt' -o 'Nox/no_refills_[480]_[memu].txt' --in-x 480 --in-y 854 --out-x 854 --out-y 480 --outtype nox
+python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/no_refills_+_timer_[480]_[memu].txt' -o 'Nox/no_refills_+_timer_[480]_[memu].txt' --in-x 480 --in-y 854 --out-x 854 --out-y 480 --outtype nox
+python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/tmrefill_[480x854]_[memu].txt' -o 'Nox/tmrefill_[480x854]_[memu].txt' --in-x 480 --in-y 854 --out-x 854 --out-y 480 --flip-y --outtype nox
+
 python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/lb_farming_(es-exit)_no_refills_720.txt' -o 'Hiro/lb_farming_(es-exit)_no_refills_720.txt' --in-x 720 --in-y 1280 --outtype hiro
 python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/no_refills_720.txt' -o 'Hiro/no_refills_720.txt' --in-x 720 --in-y 1280 --outtype hiro
 python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/no_refills_+_timer_720.txt' -o 'Hiro/no_refills_+_timer_720.txt' --in-x 720 --in-y 1280 --outtype hiro
 python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/tm_refills_720.txt' -o 'Hiro/tm_refills_720.txt' --in-x 720 --in-y 1280 --flip-y --outtype hiro
+
+python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/no_refills_[480]_[memu].txt' -o 'Hiro/no_refills_[480]_[memu].txt' --in-x 480 --in-y 854 --out-x 854 --out-y 480 --outtype hiro
+python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/no_refills_+_timer_[480]_[memu].txt' -o 'Hiro/no_refills_+_timer_[480]_[memu].txt' --in-x 480 --in-y 854 --out-x 854 --out-y 480 --outtype hiro
+python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/tmrefill_[480x854]_[memu].txt' -o 'Hiro/tmrefill_[480x854]_[memu].txt' --in-x 480 --in-y 854 --out-x 854 --out-y 480 --flip-y --outtype hiro
+
