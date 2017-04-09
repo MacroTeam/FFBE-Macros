aa9643be790fadf8264659a5ac5833f6e5bff812
kastyr <kastyr1984@gmail.com>
2017-04-08 20:51:59 -0400

    Added in old nox versions

--- Kawoxux/Exploration Automation - Nox/MagicLibrary/convert.sh
+++ Kawoxux/Exploration Automation - Nox/MagicLibrary/convert.sh
@@ -1,1 +1,5 @@
-python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/Magic Library.txt" -o "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/Magic Library-MEmu.txt" --outtype MEmu
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/Magic Library.txt" -o "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/Magic Library-MEmu.txt" --outtype memu
+
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/Magic Library.txt" -o "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/Magic Library-oldnox.txt" --outtype nox
+
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/ML Full v0.1" -o "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/ML Full v0.1-oldnox" --outtype nox
