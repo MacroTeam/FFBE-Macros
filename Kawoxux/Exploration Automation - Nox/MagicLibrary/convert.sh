db008b7c9640c8fc1b8ede7f13eafaeea0055e97
kastyr <kastyr1984@gmail.com>
2017-04-08 20:35:27 -0400

    Added in conversion and MEmu

---
+++ Kawoxux/Exploration Automation - Nox/MagicLibrary/convert.sh
@@ -0,0 +1,1 @@
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/Magic Library.txt" -o "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/Magic Library-MEmu.txt" --outtype MEmu
