7ce6836b099e662c927d87e211367558f51bf922
kastyr <kastyr1984@gmail.com>
2017-04-09 16:56:58 -0400

    Modified merge code and removed last click from others.

--- Kawoxux/Exploration Automation - Nox/MagicLibrary/convert.sh
+++ Kawoxux/Exploration Automation - Nox/MagicLibrary/convert.sh
@@ -3,3 +3,5 @@ python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mike
 python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/Magic Library.txt" -o "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/Magic Library-oldnox.txt" --outtype nox
 
 python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/ML Full v0.1" -o "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/ML Full v0.1-oldnox" --outtype nox
+
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/5a6d178973e94fa1a35f9fa874c73e62" -o "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/5a6d178973e94fa1a35f9fa874c73e62-MEmu.txt" --outtype memu 
