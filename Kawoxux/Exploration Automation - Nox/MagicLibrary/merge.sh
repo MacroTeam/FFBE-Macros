88d5c5965b0ad45b06171d8488dbeaa18e4b1354
kastyr <kastyr1984@gmail.com>
2017-04-08 20:17:43 -0400

    Added merge script and basic merge macro with no repeating wiggle

---
+++ Kawoxux/Exploration Automation - Nox/MagicLibrary/merge.sh
@@ -0,0 +1,20 @@
+#usage: mergeMacros.py [-h] -i [INFILE] MERGEFILES [MERGEFILES ...]
+#                      [-o [OUTFILE]] [-k [KEYMAPFILE]] [-l MERGELINE]
+#                      [--in-x INXREZ] [--in-y INYREZ] [--merge-x MERGEXREZ]
+#                      [--merge-y MERGEYREZ] [--out-x OUTXREZ]
+#                      [--out-y OUTYREZ] --outtype OUTTYPE [--new-nox]
+#mergeMacros.py: error: the following arguments are required: -i, -m, --outtype
+
+
+#"/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox"
+#1.Stage Selection->ExplorationOpen
+#2.4. ClearZone
+#6. Boss
+#8. Exit&Return
+
+#"/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary"
+#ML Nav1
+#ML Nav2
+
+
+python3 /home/mikey/Documents/code/convertMacros/mergeMacros.py -i "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/1.Stage Selection->ExplorationOpen" -m "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/2.4. ClearZone" "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/ML Nav1" "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/2.4. ClearZone" "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/ML Nav2" "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/6. Boss" "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/8. Exit&Return" -o "/home/mikey/Documents/code/mlmacro/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary/Magic Library.txt" --outtype nox --new-nox
