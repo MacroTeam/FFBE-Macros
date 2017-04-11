e7310d101716b9247dfb414c025fddf8579cc173
kastyr <kastyr1984@gmail.com>
2017-04-11 12:34:40 -0400

    Removed biribussi's contributions per his request.

--- biribussi/convert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#!/bin/bash
-
-python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/autobattle-for-explorations-memu720.txt' -o 'Nox/autobattle-for-explorations-memu720.txt' --outtype nox
-
-python3 ~/Documents/code/convertMacros/convertMacros.py -i 'MEmu/autobattle-for-explorations-memu720.txt' -o 'Hiro/autobattle-for-explorations-memu720.txt' --outtype hiro
