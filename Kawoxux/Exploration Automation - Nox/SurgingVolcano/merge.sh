8a93e179b5b64ec9ef14415fccdac109f2232558
kastyr <kastyr1984@gmail.com>
2017-04-14 18:12:40 -0400

    Merged and converted SV exploration

---
+++ Kawoxux/Exploration Automation - Nox/SurgingVolcano/merge.sh
@@ -0,0 +1,40 @@
+#usage: mergeMacros.py [-h] -i [INFILE] MERGEFILES [MERGEFILES ...]
+#                      [-o [OUTFILE]] [-k [KEYMAPFILE]] [-l MERGELINE]
+#                      [--in-x INXREZ] [--in-y INYREZ] [--merge-x MERGEXREZ]
+#                      [--merge-y MERGEYREZ] [--out-x OUTXREZ]
+#                      [--out-y OUTYREZ] --outtype OUTTYPE [--new-nox]
+#mergeMacros.py: error: the following arguments are required: -i, -m, --outtype
+
+
+#"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox"
+#1.Stage Selection->ExplorationOpen
+#2.4. ClearZone
+#6. Boss
+#8. Exit&Return.v2.NewNox
+
+#"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/MagicLibrary"
+#ML Nav1
+#ML Nav2
+
+python3 /home/mikey/Documents/code/convertMacros/mergeMacros.py \
+-i "/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/0.0 Clear Daily Pop-up" -m \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/1.Stage Selection->ExplorationOpen" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/2.4. ClearZone" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/2.4. ClearZone" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/2.4. ClearZone" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/2.4. ClearZone" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/2.4. ClearZone" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/2.4. ClearZone" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/SurgingVolcano/SV-Nav1.v1" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/2.4. ClearZone" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/2.4. ClearZone" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/2.4. ClearZone" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/2.4. ClearZone" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/2.4. ClearZone" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/2.4. ClearZone" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/SurgingVolcano/SV-Nav2.v1" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/6. Boss" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/X. 4s Delay" \
+"/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/8. Exit&Return.v2.NewNox" \
+-o "/home/mikey/Documents/code/head/sv/FFBE-Macros/Kawoxux/Exploration Automation - Nox/SurgingVolcano/Surging Volcano.txt" \
+--outtype nox --new-nox
