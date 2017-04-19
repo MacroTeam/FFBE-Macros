4fc194f85dff4a935e1e205146ac25b6a963caa5
kastyr <kastyr1984@gmail.com>
2017-04-18 23:32:58 -0400

    Converted egg crafting macros

--- Kawoxux/UsefulScripts/convert.sh
+++ Kawoxux/UsefulScripts/convert.sh
@@ -1,27 +1,47 @@
-python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-Nox" -o "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-MEmu.txt" --outtype memu
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-Nox" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-MEmu.txt" --outtype memu
 
-python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-Nox" -o "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-oldnox.txt" --outtype nox
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-Nox" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-oldnox.txt" --outtype nox
 
-python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-Nox" -o "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-hiro.txt" --outtype hiro
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-Nox" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-hiro.txt" --outtype hiro
 
-python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-Nox" -o "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-autotouch.txt" --outtype autotouch
-
-
-
-python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-NewNox.v1" -o "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-MEmu.v1.txt" --outtype memu
-
-python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-NewNox.v1" -o "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-oldnox.v1.txt" --outtype nox
-
-python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-NewNox.v1" -o "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-hiro.v1.txt" --outtype hiro
-
-python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-NewNox.v1" -o "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-autotouch.v1.txt" --outtype autotouch
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-Nox" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/EggSummons.v1.N-autotouch.txt" --outtype autotouch
 
 
 
-python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-Nox" -o "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-MEmu.txt" --outtype memu
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-NewNox.v1" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-MEmu.v1.txt" --outtype memu
 
-python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-Nox" -o "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-oldnox.txt" --outtype nox
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-NewNox.v1" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-oldnox.v1.txt" --outtype nox
 
-python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-Nox" -o "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-hiro.txt" --outtype hiro
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-NewNox.v1" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-hiro.v1.txt" --outtype hiro
 
-python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-Nox" -o "/home/mikey/Documents/code/head/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-autotouch.txt" --outtype autotouch
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-NewNox.v1" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FP-Gift-Accept-autotouch.v1.txt" --outtype autotouch
+
+
+
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-Nox" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-MEmu.txt" --outtype memu
+
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-Nox" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-oldnox.txt" --outtype nox
+
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-Nox" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-hiro.txt" --outtype hiro
+
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-Nox" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/FPSummons.v1.N-autotouch.txt" --outtype autotouch
+
+
+
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/GreatRadiantEggCraft.txt" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/GreatRadiantEggCraft-MEmu.txt" --outtype memu
+
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/GreatRadiantEggCraft.txt" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/GreatRadiantEggCraft-oldnox.txt" --outtype nox
+
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/GreatRadiantEggCraft.txt" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/GreatRadiantEggCraft-hiro.txt" --outtype hiro
+
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/GreatRadiantEggCraft.txt" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/GreatRadiantEggCraft-autotouch.txt" --outtype autotouch
+
+
+
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/ChocMagicEggCraft.txt" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/ChocMagicEggCraft-MEmu.txt" --outtype memu
+
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/ChocMagicEggCraft.txt" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/ChocMagicEggCraft-oldnox.txt" --outtype nox
+
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/ChocMagicEggCraft.txt" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/ChocMagicEggCraft-hiro.txt" --outtype hiro
+
+python3 /home/mikey/Documents/code/convertMacros/convertMacros.py -i "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/ChocMagicEggCraft.txt" -o "/home/mikey/Documents/code/FFBE-Macros/Kawoxux/UsefulScripts/ChocMagicEggCraft-autotouch.txt" --outtype autotouch
